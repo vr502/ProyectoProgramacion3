21798a24631aa83fc78a4844593ad69e2992d8d3
undermaster1923 <forosprogramacion502@gmail.com>
2024-05-25 16:11:27 -0600

    registro de usurio desde la pagina index

--- src/main/resources/static/js/registerUsuario.js
+++ src/main/resources/static/js/registerUsuario.js
@@ -1,21 +1,19 @@
 document.getElementById('registerForm').addEventListener('submit', async function(event) {
     event.preventDefault();
-
     let datos = {};
-    datos.Nombres = document.getElementById("firstname").value;
-    datos.Apellidos = document.getElementById("lastname").value;
-    datos.Email = document.getElementById("registerEmail").value;
-    datos.phone = document.getElementById("phone").value;
-    datos.password = document.getElementById("registerPassword").value;
-    let confirmPassword = document.getElementById("confirmPassword").value;
+    datos.nombre = document.getElementById("firstname").value;
+    datos.apellido = document.getElementById("lastname").value;
+    datos.email = document.getElementById("email").value;
+    datos.telefono = document.getElementById("phone").value;
+    datos.password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password").value;
 
     if (confirmPassword !== datos.password) {
         alert('Las contraseñas no coinciden');
         return;
     }
-
     // Simulación de registro (en un escenario real, realizar la verificación del lado del servidor)
-     await fetch('api/registro', {
+    await fetch('api/registro', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -25,27 +23,8 @@ document.getElementById('registerForm').addEventListener('submit', async functio
     });
 
     alert("Usuario creado exitosamente");
-    window.location.href = 'login.html'
-});
+    cargarUsuarios();
+    document.getElementById('registerForm').reset();
 
-document.getElementById('toggleRegisterPassword').addEventListener('click', function() {
-    const passwordField = document.getElementById('registerPassword');
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-        this.textContent = 'Ocultar';
-    } else {
-        passwordField.type = 'password';
-        this.textContent = 'Mostrar';
-    }
-});
+})
 
-document.getElementById('toggleConfirmPassword').addEventListener('click', function() {
-    const confirmPasswordField = document.getElementById('confirmPassword');
-    if (confirmPasswordField.type === 'password') {
-        confirmPasswordField.type = 'text';
-        this.textContent = 'Ocultar';
-    } else {
-        confirmPasswordField.type = 'password';
-        this.textContent = 'Mostrar';
-    }
-});
